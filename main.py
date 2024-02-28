8dea7f8086928c28a1ae7366b783238738583879
Veregorn <rjbarco@gmail.com>
2024-02-28 12:35:01 +0100

    Now the list updates every time we learn a word and save progress in a CSV file

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ WHITE = "#FFFFFF"
 
 # ------------------------------------ GLOBAL VARIABLES ------------------------------------ #
 # Read the words file and save therm in a variable (DataFrame)
-data = pandas.read_csv('data/en_es_words.csv')
+# We have to test if 'words_to_learn.csv' exist (an updated list). In that case, read from it
+try:
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    data = pandas.read_csv('data/en_es_words.csv')
 
 # Create an English-Spanish dictionary from the DataFrame
 # data_dict = {row.english:row.spanish for (index, row) in data.iterrows()} # This is a way viewed in the NATO Alphabet Project. Let's try another one here to create a list of dictionaries
@@ -23,27 +27,35 @@ selected_language = ""
 # A variable for the timer of 3 seconds to flip the card
 timer = None
 
+# We need to keep the actual word in case it needs to be removed from the list. It's a dictionary {'english': 'english_word', 'spanish': 'spanish_word'}
+current_word = {}
+
 # ------------------------------------ FLIP CARD FUNCTION ------------------------------------ #
-def flip_card(word):
+def flip_card():
     global selected_language
+    global current_word
 
     # Change the language
     if selected_language == "English":
         selected_language = "Spanish"
+        # Change the text in the canvas
+        canvas.itemconfig(word_label_id, text=current_word['spanish'], fill=WHITE)
     else:
         selected_language = "English"
+        # Change the text in the canvas
+        canvas.itemconfig(word_label_id, text=current_word['english'], fill=WHITE)
 
     # Change the image in the canvas
     canvas.itemconfig(card_id, image=card_back_img)
 
     # Change the texts in the canvas
     canvas.itemconfig(language_label_id, text=selected_language, fill=WHITE)
-    canvas.itemconfig(word_label_id, text=word, fill=WHITE)
 
 # ------------------------------------ GENERATE RANDOM ENGLISH WORDS ------------------------------------ #
 def generate_random_word():
     global selected_language
     global timer
+    global current_word
 
     # Cancel the after loop if is set so we only have on timer running!!!
     if timer:
@@ -53,11 +65,7 @@ def generate_random_word():
     canvas.itemconfig(card_id, image=card_front_img)
 
     # Pick a random dict from our 'to_learn' list
-    random_word = choice(to_learn)
-
-    # Extract both words (English and Spanish)
-    english_word = random_word['english']
-    spanish_word = random_word['spanish']
+    current_word = choice(to_learn)
     
     # Throw the coin to select a language
     selected_language = choice(HEADS_OR_TAILS)
@@ -66,15 +74,31 @@ def generate_random_word():
     if selected_language == "English":
         # Change texts in canvas
         canvas.itemconfig(language_label_id, text=selected_language, fill=BLACK)
-        canvas.itemconfig(word_label_id, text=english_word, fill=BLACK)
+        canvas.itemconfig(word_label_id, text=current_word['english'], fill=BLACK)
         # After 3 seconds, call 'flip_card' function
-        timer = window.after(3000, flip_card, spanish_word)
+        timer = window.after(3000, flip_card)
     else: # Spanish language selected
         # Change texts in canvas
         canvas.itemconfig(language_label_id, text=selected_language, fill=BLACK)
-        canvas.itemconfig(word_label_id, text=spanish_word, fill=BLACK)
+        canvas.itemconfig(word_label_id, text=current_word["spanish"], fill=BLACK)
         # After 3 seconds, call 'flip_card' function
-        timer = window.after(3000, flip_card, english_word)
+        timer = window.after(3000, flip_card)
+
+# ------------------------------------ UPDATE THE LIST AND SAVE IT TO A FILE ------------------------------------ #
+def update_list():
+    global current_word
+    global to_learn
+
+    # Remove this dict from out list
+    to_learn.remove(current_word)
+    print(f"Words to learn: {len(to_learn)}")
+
+    # Create a new CSV file, we need to create a new DataFrame first
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+    
+    # Finally, we need to generate another word
+    generate_random_word()
 
 # ------------------------------------ UI SETUP ------------------------------------ #
 # Create the App window
@@ -111,7 +135,7 @@ wrong_button.grid(column=0, row=1)
 
 # Create the 'Right' button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, width=96, height=95, highlightthickness=0, border=0, command=generate_random_word)
+right_button = Button(image=right_image, width=96, height=95, highlightthickness=0, border=0, command=update_list)
 right_button.grid(column=1, row=1)
 
 # Call for a word one first time
